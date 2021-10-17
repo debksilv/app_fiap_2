dedd1039b6f4bc41f372df000f15ab77074b6ba9
debksilv <87045920+debksilv@users.noreply.github.com>
2021-10-17 08:20:57 -0300

    Desenvolvimento Get de Tarefas

--- pages/api/task.ts
+++ pages/api/task.ts
@@ -5,13 +5,14 @@ import jwtValidator from '../../middlewares/jwtValidator';
 import { Task } from '../../types/Task';
 import { TaskModel } from '../../models/TaskModel'; 
 import { UserModel } from '../../models/UserModel'; 
+import { GetTasksQueryParams } from '../../types/GetTasksQueryParams';
 
-const handler = async(req:NextApiRequest, res : NextApiResponse<DefaultResponseMsg>) =>{
+const handler = async(req:NextApiRequest, res : NextApiResponse<DefaultResponseMsg | Task[]>) =>{
     try{
         if(req.method ==='POST'){
             return await saveTask(req, res);
         }else if(req.method ==='GET'){
-            return;
+            return await getTask(req, res);
         }else if(req.method ==='PUT'){
             return;
         }else if(req.method ==='DELETE'){
@@ -25,16 +26,71 @@ const handler = async(req:NextApiRequest, res : NextApiResponse<DefaultResponseM
     }
 }
 
+const getTask = async(req:NextApiRequest, res : NextApiResponse<DefaultResponseMsg | Task[]>) =>{
+    const userId = req?.body?.userId ? req?.body?.userId : req?.query?.userId as string;
+    const failedtValidation = await validateUser(userId);
+    if(failedtValidation){
+        return res.status(400).json({ error: failedtValidation});
+    }
+
+    const params = req.query as GetTasksQueryParams;
+
+    const query = {
+        userId
+    } as any;
+
+    if(params?.finishPrevisionStart){
+        const inputDate = new Date(params?.finishPrevisionStart);
+        query.finishPrevisionDate = { $gte : inputDate}
+    }
+
+    if(params?.finishPrevisionEnd){
+        const lastDate = new Date(params?.finishPrevisionEnd);
+        if(!query.finishPrevisionDate){
+            query.finishPrevisionDate = {};
+        }
+        query.finishPrevisionDate.$lte = lastDate
+    }
+
+    if(params?.status){
+        const status = parseInt(params?.status);
+        switch(status){
+            case 1: 
+                console.log('switch 1')
+                query.finishDate = null;
+               break;
+            case 2: 
+                console.log('switch 2')
+                query.finishDate = {$ne : null};
+               break;
+            default: break;
+        }
+    }
+
+    console.log('query', query);
+    const result = await TaskModel.find(query) as Task[];
+    console.log('result', result);
+    return res.status(200).json(result)
+}
+
+
+const validateUser = async (userId: string) =>{
+    if(!userId){
+        return 'Usuário não informado';
+    }
+
+    const userFound = await UserModel.findById(userId);
+        if(!userFound){
+            return 'Usuário não encontrado';
+        }
+}   
+
 const saveTask = async(req:NextApiRequest, res : NextApiResponse<DefaultResponseMsg>) =>{
     if(req.body){
         const userId = req.body.userId;
-        if(!userId){
-            return res.status(400).json({ error: 'Usuário não informado '});
-        }
-
-        const userFound = await UserModel.findById(userId);
-        if(!userFound){
-            return res.status(400).json({ error: 'Usuário não encontrado '}); 
+        const failedtValidation = await validateUser(userId);
+        if(failedtValidation){
+            return res.status(400).json({ error: failedtValidation});
         }
 
         const task = req.body as Task;
